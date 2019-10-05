88b480889c035a09f289fe432421998b9b260045
skhiearth <skhiearth@gmail.com>
2019-10-05 22:59:44 +0530

    poly reg

---
+++ Polynomial Regression.R
@@ -0,0 +1,34 @@
+# Polynomial Regression
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Fitting Polynomial Regression to the dataset
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+poly_reg = lm(formula = Salary ~ ., data = dataset)
+
+# Visualising the Polynomial Regression results
+library(ggplot2)
+ggplot() + geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)), colour = 'blue') +
+  xlab('Level') + ylab('Salary')
+
+# Visualising the Regression Model results (for higher resolution and smoother curve)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(poly_reg,
+                                        newdata = data.frame(Level = x_grid,
+                                                             Level2 = x_grid^2,
+                                                             Level3 = x_grid^3,
+                                                             Level4 = x_grid^4))), colour = 'blue') + 
+  xlab('Level') + ylab('Salary')
+
+# Predicting a new result with Polynomial Regression
+predict(poly_reg, data.frame(Level = 6.5,
+                             Level2 = 6.5^2,
+                             Level3 = 6.5^3,
+                             Level4 = 6.5^4))
